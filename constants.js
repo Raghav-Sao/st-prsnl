080d8beebc0d6b6a333d517ed0fa1eef0b0cda5c
Raghav <raghavendra.sao@mfine.co>
2019-12-18 23:41:05 +0530

    chardId for dec monthly done 12100-12400 for CE & PE

--- constants.js
+++ constants.js
@@ -6,72 +6,71 @@ const charts = {
 
 module.exports = {
     API_KEY: "a062yq2tywqk1e4j",
-    API_SECRET:"im93kns7sjst8ylg8y7ubypjtmz4dqgr",
+    API_SECRET: "im93kns7sjst8ylg8y7ubypjtmz4dqgr",
     REQUEST_TOKEN: "fF0QTR6hAJzSyAFnkjWUL2iKT5wNdyzb",
-    CALL_WEEKLY: {chartId: 11300098, symbol: "NIFTY19D1912250CE"},
-    PUT_WEEKLY: {chartId: 11298818, symbol: "NIFTY19D1912150PE"},
+    CALL_WEEKLY: { chartId: 11300098, symbol: "NIFTY19D1912250CE" },
+    PUT_WEEKLY: { chartId: 11298818, symbol: "NIFTY19D1912150PE" },
     NIFTY: charts.NIFTY,
-    chartByStrike:  {
+    chartByStrike: {
         call: {
-            11950: {
-                chartId: 11296002,
-                symbol: 'NIFTY19D1911950CE',
-            },
-            12000: {
-                chartId: 11296514,
-                symbol: 'NIFTY19D1912000CE',
-            },
-            12050: {
-                chartId: 11297026,
-                symbol: 'NIFTY19D1912050CE',
-            },
             12100: {
-                chartId: 11298050,
-                symbol: 'NIFTY19D1912100CE',
+                chartId: 9505794,
+                symbol: 'NIFTY19DEC12100CE',
             },
             12150: {
-                chartId: 11298562,
-                symbol: 'NIFTY19D1912150CE',
+                chartId: 13257986,
+                symbol: 'NIFTY19DEC12150CE',
             },
             12200: {
-                chartId: 11299586,
-                symbol: 'NIFTY19D1912200CE'
+                chartId: 10481922,
+                symbol: 'NIFTY19DEC12200CE'
             },
             12250: {
-                chartId: 11300098,
-                symbol: 'NIFTY19D1912250CE'
+                chartId: 13275138,
+                symbol: 'NIFTY19DEC12250CE'
+            },
+            12300: {
+                chartId: 11129858,
+                symbol: 'NIFTY19DEC12300CE'
+            },
+            12350: {
+                chartId: 13276418,
+                symbol: 'NIFTY19DEC12350CE'
+            },
+            12400: {
+                chartId: 10292994,
+                symbol: 'NIFTY19DEC12400CE'
             },
         },
         put: {
-            11950: {
-                chartId: 11296258,
-                symbol: 'NIFTY19D1911950PE',
-            },
-            12000: {
-                chartId: 11296770,
-                symbol: 'NIFTY19D1912000PE',
-            },
-            12050: {
-                chartId: 11297282,
-                symbol: 'NIFTY19D1912050PE',
-            },
             12100: {
-                chartId: 11298306,
-                symbol: 'NIFTY19D1912100PE',
+                chartId: 9506562,
+                symbol: 'NIFTY19DEC12100PE',
             },
             12150: {
-                chartId: 11298818,
-                symbol: 'NIFTY19D1912150PE',
+                chartId: 13273858,
+                symbol: 'NIFTY19DEC12150PE',
             },
             12200: {
-                chartId: 11299842,
-                symbol: 'NIFTY19D1912200PE'
+                chartId: 10483458,
+                symbol: 'NIFTY19DEC12200PE'
             },
             12250: {
-                chartId: 11300354,
-                symbol: 'NIFTY19D1912250PE'
+                chartId: 13275394,
+                symbol: 'NIFTY19DEC12250PE'
             },
-
+            12300: {
+                chartId: 11130114,
+                symbol: 'NIFTY19DEC12300PE'
+            },
+            12350: {
+                chartId: 13277442,
+                symbol: 'NIFTY19DEC12350PE'
+            },
+            12400: {
+                chartId: 10294018,
+                symbol: 'NIFTY19DEC12400PE'
+            },
         }
     }
 
